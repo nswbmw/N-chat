b39ec57d406090906e82b86d119150fe6f6e499f
nswbmw <gxqzk@126.com>
2013-09-06 18:55:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -29,71 +29,71 @@ if ('development' == app.get('env')) {
 }
 
 var users = [];//存储在线用户列表
-app.get('/', function(req,res){
-  if(req.cookies.user == null){
+app.get('/', function (req, res) {
+  if (req.cookies.user == null) {
     //注意要有 return
     return res.redirect('/signin');
   }
   res.sendfile('views/index.html');
 });
-app.get('/signin',function(req,res){
+app.get('/signin', function (req, res) {
   res.sendfile('views/signin.html');
 });
-app.post('/signin',function(req,res){
+app.post('/signin', function (req, res) {
   //检测该用户名是否已经存在于 users 数组中
-  if(users.indexOf(req.body.name) != -1){
+  if (users.indexOf(req.body.name) != -1) {
     //存在，则不允许登陆
     res.redirect('/signin');
   } else {
 	  //不存在，把用户名存入 cookie 并跳转到主页
-    res.cookie("user",req.body.name,{maxAge:1000*60*60*24*30});
+    res.cookie("user", req.body.name, {maxAge: 1000*60*60*24*30});
     res.redirect('/');
   }
 });
 
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
-io.sockets.on('connection',function(socket){
+io.sockets.on('connection', function (socket) {
 
   //有人上线
-  socket.on('online',function(data){
+  socket.on('online', function (data) {
     //将上线的用户名存储为 socket 对象的属性，以区分每个 socket 对象，方便后面使用
     socket.name = data.user;
     //数组中不存在该用户名则插入该用户名
-    if(users.indexOf(data.user) == -1){
+    if (users.indexOf(data.user) == -1) {
       users.unshift(data.user);
     }
     //向所有用户广播该用户上线信息
-    io.sockets.emit('online',{users:users,user:data.user});
+    io.sockets.emit('online', {users: users, user: data.user});
   });
 
   //有人发话
-  socket.on('say',function(data){
-    if(data.to == 'all'){
+  socket.on('say', function (data) {
+    if (data.to == 'all') {
       //向其他所有用户广播该用户发话信息
-      socket.broadcast.emit('say',data);
+      socket.broadcast.emit('say', data);
     } else {
       //向特定用户发送该用户发话信息
       //clients 为存储所有连接对象的数组
       var clients = io.sockets.clients();
       //遍历找到该用户
-      clients.forEach(function(client){
-        if(client.name == data.to){
+      clients.forEach(function (client) {
+        if (client.name == data.to) {
           //触发该用户客户端的 say 事件
-          client.emit('say',data);
+          client.emit('say', data);
         }
       });
     }
   });
 
   //有人下线
-  socket.on('disconnect',function(){
+  socket.on('disconnect', function() {
     //若 users 数组中保存了该用户名
-    if(users.indexOf(socket.name) != -1){
+    if (users.indexOf(socket.name) != -1) {
       //从 users 数组中删除该用户名
-      users.splice(users.indexOf(socket.name),1);
+      users.splice(users.indexOf(socket.name), 1);
       //向其他所有用户广播该用户下线信息
-      socket.broadcast.emit('offline',{users:users,user:socket.name});
+      socket.broadcast.emit('offline', {users: users, user: socket.name});
     }
   });
 });
